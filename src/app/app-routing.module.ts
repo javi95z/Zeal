2aa02a78949abff9d4a7bd98151272522035ff66
Javier M <javi.95.zgz@gmail.com>
2019-11-07 11:22:58 +0100

    Minor changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,27 @@
-import { ContentComponent } from './pages/layout/content/content.component';
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { AuthGuard } from './guards';
-import { LoginComponent } from './pages/auth/login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ContentComponent } from "./pages/layout/content/content.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AuthGuard } from "./guards";
+import { LoginComponent } from "./pages/auth/login/login.component";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+  { path: "login", component: LoginComponent },
   {
-    path: 'content',
+    path: "content",
     component: ContentComponent,
-    canActivate: [ AuthGuard ],
+    canActivate: [AuthGuard],
     children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
+      { path: "dashboard", component: DashboardComponent },
+      { path: "**", redirectTo: "dashboard", pathMatch: "full" }
     ]
   },
-  { path: 'admin', loadChildren: './pages/admin/admin.module#AdminModule' },
-  { path: '', redirectTo: 'content/dashboard', pathMatch: 'full' }
+  { path: "admin", loadChildren: "./pages/admin/admin.module#AdminModule" },
+  { path: "", redirectTo: "content/dashboard", pathMatch: "full" }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
