3244b7ef1c6dae86118878c19a425e9bdd257193
Javier M <javi.95.zgz@gmail.com>
2019-11-07 12:27:03 +0100

    Minor changes

--- src/app/pages/admin/users/users.component.ts
+++ src/app/pages/admin/users/users.component.ts
@@ -69,10 +69,7 @@ export class UsersAdminComponent implements OnInit {
     });
 
     dialogRef.afterClosed().subscribe((result: User) => {
-      if (result) {
-        console.log(result);
-        this.updateUser(result);
-      }
+      if (result) this.updateUser(result);
     });
   }
 
