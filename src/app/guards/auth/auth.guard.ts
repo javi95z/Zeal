d35f51757b399d916fcd1ba54fdf80bb4b0f3cf3
Javier M <javi.95.zgz@gmail.com>
2019-11-13 10:54:07 +0100

    Close all dialogs on logging out. Fixes #8

--- src/app/guards/auth/auth.guard.ts
+++ src/app/guards/auth/auth.guard.ts
@@ -1,19 +1,16 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
-import { AuthService } from '../../services';
+import { Injectable } from "@angular/core";
+import { CanActivate, Router } from "@angular/router";
+import { AuthService } from "../../services";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthGuard implements CanActivate {
-
-  constructor(
-    private auth: AuthService,
-    private router: Router) { }
+  constructor(private auth: AuthService, private router: Router) {}
 
   canActivate(): boolean {
     if (!this.auth.isLoggedIn) {
-      this.router.navigate(['/login']);
+      this.router.navigate(["/login"]);
       return false;
     }
     return true;
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,24 +1,27 @@
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { HttpClient } from "@angular/common/http";
+import { MatDialog } from "@angular/material";
+import { User } from "../../models";
 import { environment as env } from "../../../environments/environment";
-import {User} from "../../models";
 
 @Injectable({
   providedIn: "root"
 })
 export class AuthService {
-
   userData: User;
 
-  get token(): string { return sessionStorage.getItem("token"); }
-  get isLoggedIn(): boolean { return !!this.token; }
+  get token(): string {
+    return sessionStorage.getItem("token");
+  }
+  get isLoggedIn(): boolean {
+    return !!this.token;
+  }
   get currentUser(): Promise<User> {
-    return new Promise((resolve) => {
+    return new Promise(resolve => {
       if (!this.userData) {
-        this.getUser().then(res => resolve(this.userData = res));
-      }
-      else {
+        this.getUser().then(res => resolve((this.userData = res)));
+      } else {
         resolve(this.userData);
       }
     });
@@ -26,7 +29,9 @@ export class AuthService {
 
   constructor(
     private http: HttpClient,
-    private router: Router) { }
+    private router: Router,
+    private dialog: MatDialog
+  ) {}
 
   /**
    * Login with credentials
@@ -34,10 +39,10 @@ export class AuthService {
    */
   doLogin(loginData): Promise<any> {
     return new Promise((resolve, reject) => {
-      this.http.post(env.urlApi + "/auth/login", loginData)
-          .toPromise()
-          .then(res => resolve(res),
-            rej => reject(rej));
+      this.http
+        .post(env.urlApi + "/auth/login", loginData)
+        .toPromise()
+        .then(res => resolve(res), rej => reject(rej));
     });
   }
 
@@ -46,6 +51,7 @@ export class AuthService {
    */
   doLogout() {
     this.http.post(`${env.urlApi}/auth/logout`, null);
+    this.dialog.closeAll();
     this.router.navigate(["/login"]);
   }
 
@@ -54,9 +60,10 @@ export class AuthService {
    */
   getUser(): Promise<User> {
     return new Promise((resolve, reject) => {
-      this.http.post<User>(`${env.urlApi}/auth/me`, null)
-          .toPromise()
-          .then(res => resolve(res));
+      this.http
+        .post<User>(`${env.urlApi}/auth/me`, null)
+        .toPromise()
+        .then(res => resolve(res));
     });
   }
 }
