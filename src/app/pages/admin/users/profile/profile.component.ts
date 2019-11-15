e68b16311d121724b26807523207f2feed17ab36
Javier M <javi.95.zgz@gmail.com>
2019-11-15 13:21:35 +0100

    Circular dependency fix

--- src/app/pages/admin/users/profile/profile.component.ts
+++ src/app/pages/admin/users/profile/profile.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { MatDialog } from "@angular/material";
-import { EditUserDialog } from "@pages/admin/users";
+import { EditUserDialog } from "@pages/admin/users/edit-dialog/edit-dialog.component";
 import { UserService, ToastService } from "@services";
 import { User } from "@models";
 
