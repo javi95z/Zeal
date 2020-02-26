6184cdef17cccc23d99d2d7ab9cde0a9d67bcfa3
Javier M <javi.95.zgz@gmail.com>
2020-02-26 09:26:41 +0100

    Default date locale for Angular Material

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,6 +4,7 @@ import { MatCheckboxModule } from "@angular/material/checkbox";
 import { MatDatepickerModule } from "@angular/material/datepicker";
 import { MatDialogModule } from "@angular/material/dialog";
 import { MatInputModule } from "@angular/material/input";
+import { MatNativeDateModule } from "@angular/material/core";
 import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { MatSelectModule } from "@angular/material/select";
@@ -11,14 +12,22 @@ import { MatSortModule } from "@angular/material/sort";
 import { MatTableModule } from "@angular/material/table";
 import { MatTabsModule } from "@angular/material/tabs";
 import { MatTooltipModule } from "@angular/material/tooltip";
+import { MAT_DATE_LOCALE } from "@angular/material/core";
 
 @NgModule({
+  providers: [
+    {
+      provide: MAT_DATE_LOCALE,
+      useValue: "en-GB"
+    }
+  ],
   exports: [
     MatButtonModule,
     MatCheckboxModule,
     MatDatepickerModule,
     MatDialogModule,
     MatInputModule,
+    MatNativeDateModule,
     MatPaginatorModule,
     MatProgressSpinnerModule,
     MatSelectModule,
