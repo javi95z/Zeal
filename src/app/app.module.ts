41b3c659f49007bb405c33ca90d94a3e8bd39a33
Javier M <javi.95.zgz@gmail.com>
2020-02-28 10:29:08 +0100

    Register locales for future Spanish and French languages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { registerLocaleData } from "@angular/common";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { RequestInterceptor } from "@guards";
 import { AppRoutingModule } from "./app-routing.module";
@@ -17,6 +18,12 @@ import { CoreModule } from "@core/core.module";
 import { LayoutModule } from "@pages/layout/layout.module";
 import { AdminModule } from "@pages/admin/admin.module";
 import { LoginModule } from "@pages/auth/login.module";
+
+// Locales
+import localeEs from "@angular/common/locales/es";
+import localeFr from "@angular/common/locales/fr";
+registerLocaleData(localeEs, "es");
+registerLocaleData(localeFr, "fr");
 
 const httpProvider = {
   provide: HTTP_INTERCEPTORS,
