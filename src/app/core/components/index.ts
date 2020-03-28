6100769ef155d0b9391e487ae3f07a07f4f121f8
Javier M <javi.95.zgz@gmail.com>
2020-03-28 12:55:39 +0100

    Move external assets to local. Fix #15

--- src/app/core/components/index.ts
+++ src/app/core/components/index.ts
@@ -2,7 +2,6 @@ export * from "./back-button/back-button.component";
 export * from "./confirmation-dialog/confirmation-dialog.component";
 export * from "./edit-dialog/edit-dialog.component";
 export * from "./error-message/error-message.component";
-export * from "./form-errors/form-errors.component";
 export * from "./header/header.component";
 export * from "./loading/loading.component";
 export * from "./panel-header/panel-header.component";
