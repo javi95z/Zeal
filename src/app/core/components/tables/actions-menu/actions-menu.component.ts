f7746406b76e4ec0f3bdcd34992cd2fd3cfb9efc
Javier M <javi.95.zgz@gmail.com>
2021-01-24 14:32:31 +0100

    Possibility to remove actions from table actions menu

--- src/app/core/components/tables/actions-menu/actions-menu.component.ts
+++ src/app/core/components/tables/actions-menu/actions-menu.component.ts
@@ -1,15 +1,22 @@
-import { Component, Output, Input, EventEmitter } from "@angular/core";
+import { Component, OnInit, Output, Input, EventEmitter } from "@angular/core";
 
 @Component({
   selector: "z-actions-menu",
   templateUrl: "./actions-menu.component.html",
   styleUrls: ["./actions-menu.component.scss"],
 })
-export class ActionsMenuComponent {
+export class ActionsMenuComponent implements OnInit {
   @Input() isFavorite = false;
+  @Input() hide: string[];
   @Output() event = new EventEmitter<string>();
 
-  constructor() {}
+  public actions = ["FAVORITE", "EDIT", "DELETE"];
+
+  ngOnInit(): void {
+    if (this.hide) {
+      this.actions = this.actions.filter((o) => !this.hide.includes(o));
+    }
+  }
 
   onEvent(event: string) {
     this.event.emit(event);
